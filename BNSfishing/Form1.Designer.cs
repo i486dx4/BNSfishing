ffb5e86699c69d3cf492fec7e75b45378df28a94
Mac Liu <linuzilla@gmail.com>
2021-10-25 06:59:59 +0800

    initial commit

---
+++ BNSfishing/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace BNSfishing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.processTextBox = new System.Windows.Forms.TextBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.statusTextBox = new System.Windows.Forms.TextBox();
+            this.fishingBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.xTextBox = new System.Windows.Forms.TextBox();
+            this.yTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.windowCheckBox = new System.Windows.Forms.CheckBox();
+            this.predefinedComboBox = new System.Windows.Forms.ComboBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(0, 120);
+            this.startButton.Margin = new System.Windows.Forms.Padding(2);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(242, 53);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // processTextBox
+            // 
+            this.processTextBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.processTextBox.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.processTextBox.Location = new System.Drawing.Point(0, 0);
+            this.processTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.processTextBox.Name = "processTextBox";
+            this.processTextBox.ReadOnly = true;
+            this.processTextBox.Size = new System.Drawing.Size(506, 32);
+            this.processTextBox.TabIndex = 1;
+            this.processTextBox.Text = "BNSR";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(246, 120);
+            this.stopButton.Margin = new System.Windows.Forms.Padding(2);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(260, 53);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // statusTextBox
+            // 
+            this.statusTextBox.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.statusTextBox.Location = new System.Drawing.Point(0, 177);
+            this.statusTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.statusTextBox.Multiline = true;
+            this.statusTextBox.Name = "statusTextBox";
+            this.statusTextBox.ReadOnly = true;
+            this.statusTextBox.Size = new System.Drawing.Size(506, 65);
+            this.statusTextBox.TabIndex = 3;
+            // 
+            // fishingBackgroundWorker
+            // 
+            this.fishingBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.fishingBackgroundWorker_DoWork);
+            this.fishingBackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.fishingBackgroundWorker_RunWorkerCompleted);
+            // 
+            // xTextBox
+            // 
+            this.xTextBox.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.xTextBox.Location = new System.Drawing.Point(38, 43);
+            this.xTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.xTextBox.Name = "xTextBox";
+            this.xTextBox.Size = new System.Drawing.Size(121, 32);
+            this.xTextBox.TabIndex = 4;
+            this.xTextBox.Text = "1152";
+            // 
+            // yTextBox
+            // 
+            this.yTextBox.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.yTextBox.Location = new System.Drawing.Point(216, 43);
+            this.yTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.yTextBox.Name = "yTextBox";
+            this.yTextBox.Size = new System.Drawing.Size(121, 32);
+            this.yTextBox.TabIndex = 5;
+            this.yTextBox.Text = "678";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(11, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 27);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(180, 45);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 27);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "y";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 249);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 11, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(506, 30);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(37, 25);
+            this.statusLabel.Text = "OK";
+            // 
+            // windowCheckBox
+            // 
+            this.windowCheckBox.AutoSize = true;
+            this.windowCheckBox.Location = new System.Drawing.Point(362, 46);
+            this.windowCheckBox.Name = "windowCheckBox";
+            this.windowCheckBox.Size = new System.Drawing.Size(132, 24);
+            this.windowCheckBox.TabIndex = 9;
+            this.windowCheckBox.Text = "Windows Mode";
+            this.windowCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // predefinedComboBox
+            // 
+            this.predefinedComboBox.FormattingEnabled = true;
+            this.predefinedComboBox.Items.AddRange(new object[] {
+            "1152,678",
+            "1875,929"});
+            this.predefinedComboBox.Location = new System.Drawing.Point(38, 87);
+            this.predefinedComboBox.Name = "predefinedComboBox";
+            this.predefinedComboBox.Size = new System.Drawing.Size(436, 28);
+            this.predefinedComboBox.TabIndex = 10;
+            this.predefinedComboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 279);
+            this.Controls.Add(this.predefinedComboBox);
+            this.Controls.Add(this.windowCheckBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yTextBox);
+            this.Controls.Add(this.xTextBox);
+            this.Controls.Add(this.statusTextBox);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.processTextBox);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "BNS Fishing";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox processTextBox;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TextBox statusTextBox;
+        private System.ComponentModel.BackgroundWorker fishingBackgroundWorker;
+        private System.Windows.Forms.TextBox xTextBox;
+        private System.Windows.Forms.TextBox yTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.CheckBox windowCheckBox;
+        private System.Windows.Forms.ComboBox predefinedComboBox;
+    }
+}
+
---
+++ BNSfishing/Form1.cs
@@ -0,0 +1,363 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BNSfishing {
+    public partial class Form1 : Form {
+        private bool looping = false;
+        private Bitmap screenPixel = new Bitmap(1, 1, PixelFormat.Format32bppArgb);
+        private Point samplePoint = new Point(0, 0);
+        private Random rnd = new Random();
+        private IntPtr windowPointer;
+
+        public Form1() {
+            InitializeComponent();
+            statusLabel.Text = "done.";
+        }
+
+        private IntPtr findProcess() {
+            Process[] processCollection = Process.GetProcesses();
+            foreach (Process p in processCollection) {
+                try {
+                    if (p.ProcessName == this.processTextBox.Text) {
+                        return EnumerateProcessWindowHandles(p.Id).First();
+                    }
+                } catch (Exception) {
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+        private void startButton_Click(object sender, EventArgs e) {
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+
+            // fishingBackgroundWorker.RunWorkerAsync(2000);
+            Start();
+        }
+
+        private void Start() {
+            looping = true;
+            this.windowPointer = findProcess();
+
+            /*
+            for (int i = 0; (i < 60) && (zero == IntPtr.Zero); i++)
+            {
+                Thread.Sleep(50);
+                zero = FindWindow(null, this.processTextBox.Text);
+            }
+            */
+            if (this.windowPointer != IntPtr.Zero) {
+                statusTextBox.Text = "Found";
+
+                int x = Int32.Parse(xTextBox.Text);
+                int y = Int32.Parse(yTextBox.Text);
+
+                var c = GetColorAt(this.windowPointer, x, y);
+
+                statusLabel.Text = "color: " + c.ToArgb();
+
+                // SetForegroundWindow(this.windowPointer);
+                Thread.Sleep(500);
+
+                samplePoint = new Point(x,y);
+
+                // var color = GetColorAt(zero, Int32.Parse(xTextBox.Text), Int32.Parse(ytextBox.Text));
+                var color = GetPixel(samplePoint);
+                // currentColor = color.ToArgb();
+                fishingBackgroundWorker.RunWorkerAsync(2000);
+                /*
+                while (looping)
+                {
+                    SendKeys.SendWait("{5}");
+                    Thread.Sleep(20000);
+                    SendKeys.SendWait("{f}");
+                    Thread.Sleep(1000);
+                }
+                */
+                /*
+                SendKeys.SendWait("{TAB}");
+                SendKeys.SendWait("{TAB}");
+                SendKeys.SendWait("{ENTER}");
+                SendKeys.Flush();
+                */
+            } else {
+                statusTextBox.Text = "NOT Found";
+                startButton.Enabled = true;
+                stopButton.Enabled = false;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, UInt32 Msg, int wParam, int lParam);
+
+        //[DllImport("user32.dll")]
+        //public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        //[DllImport("user32.dll")]
+        //public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        delegate bool EnumThreadDelegate(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool EnumThreadWindows(int dwThreadId, EnumThreadDelegate lpfn,
+            IntPtr lParam);
+
+        //[DllImport("user32.dll")]
+        //static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc, int ySrc, int dwRop);
+
+
+        static IEnumerable<IntPtr> EnumerateProcessWindowHandles(int processId) {
+            var handles = new List<IntPtr>();
+
+            foreach (ProcessThread thread in Process.GetProcessById(processId).Threads)
+                EnumThreadWindows(thread.Id,
+                    (hWnd, lParam) => { handles.Add(hWnd); return true; }, IntPtr.Zero);
+
+            return handles;
+        }
+
+
+        /*
+        public static Bitmap PrintWindow(IntPtr hwnd)
+        {
+            RECT rc;
+            GetWindowRect(hwnd, out rc);
+
+
+            Bitmap bmp = new Bitmap(rc.Width, rc.Height, PixelFormat.Format32bppArgb);
+            Graphics gfxBmp = Graphics.FromImage(bmp);
+            IntPtr hdcBitmap = gfxBmp.GetHdc();
+
+            PrintWindow(hwnd, hdcBitmap, 0);
+
+            gfxBmp.ReleaseHdc(hdcBitmap);
+            gfxBmp.Dispose();
+
+            return bmp;
+        }
+        */
+
+        private void stopButton_Click(object sender, EventArgs e) {
+            looping = false;
+            stopButton.Enabled = false;
+            if (!fishingBackgroundWorker.IsBusy) {
+                fishingBackgroundWorker.CancelAsync();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            statusLabel.Text = "Index: " + predefinedComboBox.SelectedIndex + ", value: " + predefinedComboBox.Text;
+
+            var list = predefinedComboBox.Text.Split(",").ToList();
+
+            xTextBox.Text = list[0].Trim();
+            yTextBox.Text = list[1].Trim();
+
+        }
+
+        private void fishingBackgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e) {
+            const int WM_SYSKEYDOWN = 0x0104;
+            // const int WM_KEYDOWN = 0x0100;
+            // const int WM_SYSKEYUP = 0x;
+            const int VK_KEY_5 = 0x35;
+            const int VK_KEY_F = 0x46;
+
+            int color1, color2;
+            int counter = 0;
+
+            var windowMode = windowCheckBox.Checked;
+
+            Thread.Sleep(1500);
+
+            while (looping && counter < 1500) {
+                // SendKeys.SendWait("{5}");
+                PostMessage(this.windowPointer, WM_SYSKEYDOWN, VK_KEY_5, 0);
+
+                Thread.Sleep(2000);
+
+                color1 = windowMode ?
+                    GetColorAt(this.windowPointer, samplePoint.X, samplePoint.Y).ToArgb() :
+                    GetPixel(samplePoint).ToArgb();
+
+                do {
+                    Thread.Sleep(500);
+                    color2 = windowMode ?
+                        GetColorAt(this.windowPointer, samplePoint.X, samplePoint.Y).ToArgb() :
+                        GetPixel(samplePoint).ToArgb();
+                } while (color1 == color2);
+                Thread.Sleep(100);
+                //SendKeys.SendWait("{f}");
+                counter++;
+                PostMessage(this.windowPointer, WM_SYSKEYDOWN, VK_KEY_F, 0);
+                statusLabel.Text = "Counter: " + counter + ",  from: " + color1 + " to " + color2;
+                Thread.Sleep(rnd.Next(50, 300) + rnd.Next(50, 4500));
+            }
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+        }
+
+        private void fishingBackgroundWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e) {
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+        }
+
+        //private void MouseMoveTimer_Tick(object sender, EventArgs e) {
+        //    /*
+        //    Point cursor = new Point();
+        //    GetCursorPos(ref cursor);
+
+        //    var c = GetColorAt(cursor);
+        //    this.BackColor = c;
+
+        //    if (c.R == c.G && c.G < 64 && c.B > 128)
+        //    {
+        //        MessageBox.Show("Blue");
+        //    }
+        //    */
+        //}
+
+        private Color GetPixel(Point position) {
+            using (var bitmap = new Bitmap(1, 1)) {
+                using (var graphics = Graphics.FromImage(bitmap)) {
+                    graphics.CopyFromScreen(position, new Point(0, 0), new Size(1, 1));
+                }
+                return bitmap.GetPixel(0, 0);
+            }
+        }
+
+        public Color GetColorAt(IntPtr hwnd, int x, int y) {
+            // statusLabel.Text = "Retrieve color at " + x + " ,  " + y;
+
+            using (Graphics gdest = Graphics.FromImage(screenPixel)) {
+                using (Graphics gsrc = Graphics.FromHwnd(hwnd)) {
+                    IntPtr hSrcDC = gsrc.GetHdc();
+                    IntPtr hDC = gdest.GetHdc();
+                    int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, x, y, (int)CopyPixelOperation.SourceCopy);
+
+
+                    // statusLabel.Text = "Return value: " + retval;
+                    gdest.ReleaseHdc();
+                    gsrc.ReleaseHdc();
+                }
+            }
+
+            return screenPixel.GetPixel(0, 0);
+        }
+    }
+/*
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT {
+        private int _Left;
+        private int _Top;
+        private int _Right;
+        private int _Bottom;
+
+        public RECT(RECT Rectangle) : this(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom) {
+        }
+        public RECT(int Left, int Top, int Right, int Bottom) {
+            _Left = Left;
+            _Top = Top;
+            _Right = Right;
+            _Bottom = Bottom;
+        }
+
+        public int X {
+            get { return _Left; }
+            set { _Left = value; }
+        }
+        public int Y {
+            get { return _Top; }
+            set { _Top = value; }
+        }
+        public int Left {
+            get { return _Left; }
+            set { _Left = value; }
+        }
+        public int Top {
+            get { return _Top; }
+            set { _Top = value; }
+        }
+        public int Right {
+            get { return _Right; }
+            set { _Right = value; }
+        }
+        public int Bottom {
+            get { return _Bottom; }
+            set { _Bottom = value; }
+        }
+        public int Height {
+            get { return _Bottom - _Top; }
+            set { _Bottom = value + _Top; }
+        }
+        public int Width {
+            get { return _Right - _Left; }
+            set { _Right = value + _Left; }
+        }
+        public Point Location {
+            get { return new Point(Left, Top); }
+            set {
+                _Left = value.X;
+                _Top = value.Y;
+            }
+        }
+        public Size Size {
+            get { return new Size(Width, Height); }
+            set {
+                _Right = value.Width + _Left;
+                _Bottom = value.Height + _Top;
+            }
+        }
+
+        public static implicit operator Rectangle(RECT Rectangle) {
+            return new Rectangle(Rectangle.Left, Rectangle.Top, Rectangle.Width, Rectangle.Height);
+        }
+        public static implicit operator RECT(Rectangle Rectangle) {
+            return new RECT(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom);
+        }
+        public static bool operator ==(RECT Rectangle1, RECT Rectangle2) {
+            return Rectangle1.Equals(Rectangle2);
+        }
+        public static bool operator !=(RECT Rectangle1, RECT Rectangle2) {
+            return !Rectangle1.Equals(Rectangle2);
+        }
+
+        public override string ToString() {
+            return "{Left: " + _Left + "; " + "Top: " + _Top + "; Right: " + _Right + "; Bottom: " + _Bottom + "}";
+        }
+
+        public override int GetHashCode() {
+            return ToString().GetHashCode();
+        }
+
+        public bool Equals(RECT Rectangle) {
+            return Rectangle.Left == _Left && Rectangle.Top == _Top && Rectangle.Right == _Right && Rectangle.Bottom == _Bottom;
+        }
+
+        public override bool Equals(object Object) {
+            if (Object is RECT) {
+                return Equals((RECT)Object);
+            } else if (Object is Rectangle) {
+                return Equals(new RECT((Rectangle)Object));
+            }
+
+            return false;
+        }
+    }*/
+}
